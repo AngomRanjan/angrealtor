0852dffab5bd1676e2ff292262abe1c5fbdc9fee
AngomRanjan <angomranjan@gmail.com>
2023-02-11 22:54:11 +0530

    Install nd Setup Firebase

--- firebase.js
+++ firebase.js
@@ -1,19 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: 'ranflix-f6810.firebaseapp.com',
-  projectId: 'ranflix-f6810',
-  storageBucket: 'ranflix-f6810.appspot.com',
+  authDomain: 'angom-realtor.firebaseapp.com',
+  projectId: 'angom-realtor',
+  storageBucket: 'angom-realtor.appspot.com',
   messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
-const auth = getAuth(firebaseApp);
-
-export { auth };
-export default db;
