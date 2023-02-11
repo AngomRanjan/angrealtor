011cfcf7305814aaf02207832798c8cabce42543
AngomRanjan <angomranjan@gmail.com>
2023-02-11 22:23:17 +0530

    Add firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: 'ranflix-f6810.firebaseapp.com',
+  projectId: 'ranflix-f6810',
+  storageBucket: 'ranflix-f6810.appspot.com',
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+
+export { auth };
+export default db;
